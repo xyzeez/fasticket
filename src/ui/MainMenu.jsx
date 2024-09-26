01b5b1c14b8882f0339ef2b4b77ece47f216a0e5
Abdu-Samii Abdul-Azeez <106898953+xyzeez@users.noreply.github.com>
2024-09-27 02:07:52 +0300

    fix(app): menu modal opening delay

--- src/ui/MainMenu.jsx
+++ src/ui/MainMenu.jsx
@@ -14,15 +14,13 @@ const MainMenu = ({ relativeClasses }) => {
     <nav className={`${relativeClasses} group flex w-fit`}>
       <button
         aria-label="show main-menu"
-        className="z-20 size-6 outline-none ring-0 ring-offset-transparent focus:ring-[hsla(273,70%,36%,1)] focus:ring-offset-1 lg:hidden"
+        className="peer z-20 size-6 lg:hidden"
         onClick={() => setShowMenu(!showMenu)}
-        onFocus={() => setShowMenu(true)}
-        onBlur={() => setShowMenu(false)}
       >
         <Hamburger isOpen={showMenu} />
       </button>
       <menu
-        className={`${showMenu ? "visible translate-y-0 scale-100 opacity-100" : "invisible translate-y-1 scale-90 opacity-0 focus-within:visible focus-within:translate-y-0 focus-within:scale-100 focus-within:opacity-100"} peer absolute left-0 right-0 top-[calc(100%+12px)] z-20 mx-4 origin-top-right rounded-xl bg-[hsla(0,0%,100%,1)] p-8 shadow-[0px_2px_4px_0px_hsla(0,0%,0%,0.08),0px_0px_6px_0px_hsla(0,0%,0%,0.02)] transition-all duration-300 lg:visible lg:relative lg:left-auto lg:right-auto lg:top-auto lg:mx-0 lg:w-auto lg:translate-y-0 lg:scale-100 lg:rounded-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none`}
+        className={`${showMenu ? "visible translate-y-0 scale-100 opacity-100" : "invisible translate-y-1 scale-90 opacity-0"} peer absolute left-0 right-0 top-[calc(100%+12px)] z-20 mx-4 origin-top-right rounded-xl bg-[hsla(0,0%,100%,1)] p-8 shadow-[0px_2px_4px_0px_hsla(0,0%,0%,0.08),0px_0px_6px_0px_hsla(0,0%,0%,0.02)] transition-all duration-300 focus-within:visible focus-within:translate-y-0 focus-within:scale-100 focus-within:opacity-100 peer-focus-visible:visible peer-focus-visible:translate-y-0 peer-focus-visible:scale-100 peer-focus-visible:opacity-100 lg:visible lg:relative lg:left-auto lg:right-auto lg:top-auto lg:mx-0 lg:w-auto lg:translate-y-0 lg:scale-100 lg:rounded-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none`}
       >
         <ul className="flex w-full flex-col gap-6 text-sm/[14px] font-medium text-[hsla(0,0%,0%,1)] lg:flex-row">
           {menus.map((item, index) => (
@@ -36,11 +34,7 @@ const MainMenu = ({ relativeClasses }) => {
         </ul>
       </menu>
       <div
-        className={`fixed inset-0 z-10 h-screen w-screen bg-black/5 backdrop-blur-2xl transition-transform duration-100 lg:hidden ${
-          showMenu
-            ? "scale-100 opacity-100"
-            : "scale-0 opacity-0 peer-focus-within:scale-100 peer-focus-within:opacity-100"
-        }`}
+        className={`${showMenu ? "scale-100 opacity-100" : "scale-0 opacity-0"} fixed inset-0 z-10 h-screen w-screen bg-black/5 backdrop-blur-2xl transition-transform duration-100 peer-focus-within:scale-100 peer-focus-within:opacity-100 peer-focus-visible:scale-100 peer-focus-visible:opacity-100 lg:hidden`}
       ></div>
     </nav>
   );
--- src/ui/UserMenu.jsx
+++ src/ui/UserMenu.jsx
@@ -7,17 +7,17 @@ const UserMenu = ({ relativeClasses }) => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className={`${relativeClasses} w-fit`}>
-      <div className="flex w-fit flex-row items-center justify-center gap-1 rounded-[51px] bg-[hsla(273,70%,94%,1)] px-2 py-1">
-        <button aria-label="change language" className="size-fit">
-          <img src="icons/saudi-arabia.svg" className="h-4 w-[21.33px]" />
-        </button>
+    <div
+      className={`${relativeClasses} flex w-fit flex-row items-center justify-center gap-1 rounded-[51px] bg-[hsla(273,70%,94%,1)] px-2 py-1`}
+    >
+      <button aria-label="change language" className="size-fit">
+        <img src="icons/saudi-arabia.svg" className="h-4 w-[21.33px]" />
+      </button>
+      <div className="w-fit">
         <button
           aria-label="show user-menu"
-          className="flex w-fit cursor-pointer flex-row items-center"
+          className="peer flex w-fit flex-row items-center"
           onClick={() => setShowMenu(!showMenu)}
-          onFocus={() => setShowMenu(true)}
-          onBlur={() => setShowMenu(false)}
         >
           <img src="icons/user.svg" className="h-6 w-6" />
           <img
@@ -25,21 +25,21 @@ const UserMenu = ({ relativeClasses }) => {
             className={`h-6 w-5 transition-all duration-500 ${showMenu ? "rotate-180" : "rotate-0"}`}
           />
         </button>
-      </div>
-      <div
-        className={`${showMenu ? "visible translate-y-0 scale-100 opacity-100" : "invisible translate-y-1 scale-90 opacity-0 focus-within:visible focus-within:translate-y-0 focus-within:scale-100 focus-within:opacity-100"} absolute top-[calc(100%+12px)] w-full max-w-[120px] origin-top-left rounded bg-[hsla(0,0%,100%,1)] p-2 shadow-[0px_2px_4px_0px_hsla(0,0%,0%,0.08),0px_0px_6px_0px_hsla(0,0%,0%,0.02)] transition-all duration-300 lg:right-0 lg:origin-top-right`}
-      >
-        <ul className="flex w-full flex-col gap-2 text-sm/[14px] text-[hsla(0,0%,0%,1)]">
-          {menus.map((item, index) => (
-            <Link
-              key={index}
-              to="/"
-              className="w-full rounded-[4px] border border-[hsla(213,20%,89%,1)] p-2 pb-[6px]"
-            >
-              {item}
-            </Link>
-          ))}
-        </ul>
+        <div
+          className={`${showMenu ? "visible translate-y-0 scale-100 opacity-100" : "invisible translate-y-1 scale-90 opacity-0"} absolute top-[calc(100%+12px)] w-full max-w-[120px] origin-top-left rounded border bg-[hsla(0,0%,100%,1)] p-2 shadow-[0px_2px_4px_0px_hsla(0,0%,0%,0.08),0px_0px_6px_0px_hsla(0,0%,0%,0.02)] transition-all duration-300 focus-within:visible focus-within:translate-y-0 focus-within:scale-100 focus-within:opacity-100 peer-focus-visible:visible peer-focus-visible:translate-y-0 peer-focus-visible:scale-100 peer-focus-visible:opacity-100 lg:right-0 lg:origin-top-right`}
+        >
+          <ul className="flex w-full flex-col gap-2 text-sm/[14px] text-[hsla(0,0%,0%,1)]">
+            {menus.map((item, index) => (
+              <Link
+                key={index}
+                to="/"
+                className="focus-ring w-full rounded-[4px] border border-[hsla(213,20%,89%,1)] p-2 pb-[6px]"
+              >
+                {item}
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
